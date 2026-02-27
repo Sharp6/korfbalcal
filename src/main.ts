4f3aa2d9a7ba1d532efb235d68caa57532b285f8
Philip Huysmans <philip.huysmans@gmail.com>
2026-02-27 13:45:16 +0100

    locale fixed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeNlBe from '@angular/common/locales/nl-BE';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+registerLocaleData(localeNlBe);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
